cbcbcb8fdabccf6bde88c55b7795b0926bb8059b
zola880 <zelalemybabe77@gmail.com>
2026-05-08 15:25:27 +0300

    hg

--- backend/src/utils/generateToken.js
+++ backend/src/utils/generateToken.js
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken';
 
 const generateToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN,
+    expiresIn: process.env.JWT_EXPIRES_IN || '7d', // fallback added
   });
 };
 
